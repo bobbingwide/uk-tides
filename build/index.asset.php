5df8660e4b15838d18adaeebf9d83f96e811df4b
Herb Miller <herb@bobbingwide.com>
2021-07-24 14:45:40 +0100

    Update build files; generated by npm run dev #9

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => 'd6b970acfede205e1732f4e6070b1038');
