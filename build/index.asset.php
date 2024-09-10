aa06008dee8455b99b2bd60e6ba22f6f51c22058
Herb Miller <herb@bobbingwide.com>
2024-09-10 16:52:21 +0100

    Update build files #23

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '993d905fbac65b90b2a8');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '94ce83b11035c5d10e4f');
